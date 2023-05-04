78ee25618733f4e4b99053034ca64fee75742a5b
callbacked <117959902+callbacked@users.noreply.github.com>
2023-05-04 13:57:10 -0400

    Create vnc-setup.sh

---
+++ vnc-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install TigerVNC and related packages
+echo "Installing TigerVNC and related packages..."
+sudo dnf install -y tigervnc-server tigervnc tigervnc-viewer
+
+# Configure the VNC server
+echo "Configuring the VNC server..."
+read -p "Enter the display number (e.g., 1): " DISPLAY_NUM
+sudo cp /usr/lib/systemd/system/vncserver@.service /etc/systemd/system/vncserver@:${DISPLAY_NUM}.service
+
+# Substitute the user in the service file
+read -p "Enter your username (e.g., alex): " USERNAME
+sudo sed -i "s|<USER>|${USERNAME}|g" /etc/systemd/system/vncserver@:${DISPLAY_NUM}.service
+
+# Reload the systemd daemon
+sudo systemctl daemon-reload
+
+# Create the VNC password
+echo "Setting up the VNC password for user ${USERNAME}..."
+sudo -u ${USERNAME} vncpasswd
+
+# Enable and start the VNC server
+echo "Enabling and starting the VNC server..."
+sudo systemctl enable vncserver@:${DISPLAY_NUM}.service
+sudo systemctl start vncserver@:${DISPLAY_NUM}.service
+
+echo "VNC server setup completed successfully."
