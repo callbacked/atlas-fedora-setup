021e409898d492e3fffdeea5427022c1bc2d4838
callbacked <117959902+callbacked@users.noreply.github.com>
2023-05-04 14:01:28 -0400

    Update vnc-setup.sh
    shoutout chatgpt

--- vnc-setup.sh
+++ vnc-setup.sh
@@ -25,4 +25,10 @@ echo "Enabling and starting the VNC server..."
 sudo systemctl enable vncserver@:${DISPLAY_NUM}.service
 sudo systemctl start vncserver@:${DISPLAY_NUM}.service
 
+# Configure the firewall to allow VNC connections
+echo "Configuring the firewall to allow VNC connections..."
+VNC_PORT=$((5900 + DISPLAY_NUM))
+sudo firewall-cmd --add-port=${VNC_PORT}/tcp --permanent
+sudo firewall-cmd --reload
+
 echo "VNC server setup completed successfully."
