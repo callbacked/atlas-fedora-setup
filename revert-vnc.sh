d4fd9f08455a26af5fe8c6c6cd32a14a469c46a3
callbacked <117959902+callbacked@users.noreply.github.com>
2023-05-04 14:08:47 -0400

    Update revert-vnc.sh

--- revert-vnc.sh
+++ revert-vnc.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-# Request confirmation before proceeding
-read -p "Are you sure you want to revert the VNC server setup? (y/N) " CONFIRM
-if [[ ! "${CONFIRM}" =~ ^[Yy]$ ]]; then
-    echo "Revert cancelled."
-    exit 0
+# Check if the user is root
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root."
+    exit 1
 fi
 
-# Stop and disable the VNC server service
-read -p "Enter the display number you used during the initial setup (e.g., 1): " DISPLAY_NUM
-sudo systemctl stop vncserver@:${DISPLAY_NUM}.service
-sudo systemctl disable vncserver@:${DISPLAY_NUM}.service
+# Stop and disable the VNC server
+systemctl disable --now vncserver@:1.service
 
-# Remove the VNC server service file
-sudo rm /etc/systemd/system/vncserver@:${DISPLAY_NUM}.service
+# Remove the VNC server configuration
+rm -f /etc/systemd/system/vncserver@.service
 
-# Reload the systemd daemon
-sudo systemctl daemon-reload
+# Reload systemd configuration
+systemctl daemon-reload
 
-# Remove the firewall rule for the VNC server
-echo "Removing the firewall rule for the VNC server..."
-VNC_PORT=$((5900 + DISPLAY_NUM))
-sudo firewall-cmd --remove-port=${VNC_PORT}/tcp --permanent
-sudo firewall-cmd --reload
+# Remove VNC server packages
+dnf remove -y tigervnc-server tigervnc
 
-echo "VNC server setup has been reverted successfully."
+# Remove firewall rule for VNC server
+firewall-cmd --remove-service=vnc-server --permanent
+firewall-cmd --reload
+
+echo "VNC server removed."
