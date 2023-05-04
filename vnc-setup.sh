e6b8847a7801bb66a3cd1b62b9c0712465ff8ef0
callbacked <117959902+callbacked@users.noreply.github.com>
2023-05-04 14:21:45 -0400

    Update vnc-setup.sh

--- vnc-setup.sh
+++ vnc-setup.sh
@@ -11,31 +11,19 @@ dnf install -y tigervnc-server tigervnc
 
 # Configure the VNC server
 read -p "Enter the username for the VNC server: " vnc_user
+read -p "Enter the display number for the VNC server (e.g., 1, 2, ...): " vnc_display
 
-cat << EOF > /etc/systemd/system/vncserver@:1.service
-[Unit]
-Description=Remote desktop service (VNC)
-After=syslog.target network.target
-
-[Service]
-Type=forking
-User=${vnc_user}
-ExecStartPre=/bin/sh -c '/usr/bin/vncserver -kill %i > /dev/null 2>&1 || :'
-ExecStart=/usr/bin/vncserver %i
-ExecStop=/usr/bin/vncserver -kill %i
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Reload systemd configuration
-systemctl daemon-reload
+# Add user mapping
+echo ":${vnc_display}=${vnc_user}" >> /etc/tigervnc/vncserver.users
 
 # Set a VNC password for the user
 su - ${vnc_user} -c "vncpasswd"
 
-# Enable and start the VNC server
-systemctl enable --now vncserver@:1.service
+# Start the VNC server
+systemctl start vncserver@:${vnc_display}
+
+# Enable the VNC server
+systemctl enable vncserver@:${vnc_display}
 
 # Add firewall rule to allow VNC traffic
 firewall-cmd --add-service=vnc-server --permanent
